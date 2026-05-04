5669f8b727ceebfc498ac38c06b02fa2ea983ad2
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-04 12:58:24 +0200

    Added webapi to create bid

--- CarBiddingPlatform.WebAPI/Controllers/BidsController.cs
+++ CarBiddingPlatform.WebAPI/Controllers/BidsController.cs
@@ -1,6 +1,32 @@
-﻿namespace CarBiddingPlatform.WebAPI.Controllers;
+﻿using CarBiddingPlatform.Application;
+using CarBiddingPlatform.Application.commands;
+using Microsoft.AspNetCore.Mvc;
 
-public class BidsController
+namespace CarBiddingPlatform.WebAPI.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class BidsController : ControllerBase
 {
-    
+    private readonly PlaceBidCommandHandler _commandHandler;
+
+    public BidsController(PlaceBidCommandHandler commandHandler)
+    {
+        _commandHandler = commandHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PlaceBid([FromBody] PlaceBidCommand command)
+    {
+        try
+        {
+            var bid = await _commandHandler.HandleAsync(command);
+            return Ok(bid);
+        }
+        catch
+        {
+            return NotFound();
+        }
+
+       
+    }
 }
---
+++ CarBiddingPlatform.WebAPI/Program.cs
@@ -0,0 +1,44 @@
+using CarBiddingPlatform.Application.commands;
+using CarBiddingPlatform.Application.Interfaces;
+using CarBiddingPlatform.Infrastructure.Data;
+using CarBiddingPlatform.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBiddingPlatform.WebAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<BiddingDbContext>(options => 
+            options.UseSqlServer(connectionString));
+        // Add services to the container.
+        
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<BiddingDbContext>();
+        builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+        builder.Services.AddScoped<PlaceBidCommandHandler>();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization(); 
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
