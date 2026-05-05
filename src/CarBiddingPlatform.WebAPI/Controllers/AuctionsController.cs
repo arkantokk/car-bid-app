fdf68cb29361e59be3921a0cf5f0de130705be0e
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-05 12:51:07 +0200

    Created a contorller with post endpoint

---
+++ src/CarBiddingPlatform.WebAPI/Controllers/AuctionsController.cs
@@ -0,0 +1,29 @@
+﻿using CarBiddingPlatform.Application.Commands.CreateAuction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBiddingPlatform.WebAPI.Controllers;
+[ApiController]
+[Route("/api/[controller]")]
+public class AuctionsController : ControllerBase
+{
+    private readonly CreateAuctionCommandHandler _createAuctionCommandHandler;
+
+    public AuctionsController(CreateAuctionCommandHandler createAuctionCommandHandler)
+    {
+        _createAuctionCommandHandler = createAuctionCommandHandler;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAuctionAsync([FromBody] CreateAuctionCommand command)
+    {
+        try
+        {
+            var auction = await _createAuctionCommandHandler.HandleAsync(command);
+            return Ok(auction);
+        }
+        catch
+        {
+            return BadRequest();
+        }
+    }
+}
