430ef9ae5ddd80246fab7ce481ddcd9e32f4dff1
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-07 12:18:37 +0200

    Feature: added a globalerrorhandler

---
+++ src/CarBiddingPlatform.WebAPI/Middlewares/GlobalErrorHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBiddingPlatform.WebAPI.Middlewares;
+
+public class GlobalErrorHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var statusCode = exception switch
+        {
+            KeyNotFoundException => StatusCodes.Status404NotFound,
+            InvalidOperationException => StatusCodes.Status400BadRequest,
+            // TODO: forbidden
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = "An error occurred while processing your request.",
+            Detail = exception.Message,
+            Instance = httpContext.Request.Path
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+        httpContext.Response.ContentType = "application/problem+json";
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
