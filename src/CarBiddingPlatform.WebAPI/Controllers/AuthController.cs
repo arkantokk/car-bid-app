859b2a0d44998e5ae881300b25d7d6f80c8e8e90
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-09 14:55:05 +0200

    Implemented Auth controller api

--- src/CarBiddingPlatform.WebAPI/Controllers/AuthController.cs
+++ src/CarBiddingPlatform.WebAPI/Controllers/AuthController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBiddingPlatform.Application.Commands.Auth.Login;
+using CarBiddingPlatform.Application.Commands.Auth.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarBiddingPlatform.WebAPI.Controllers;
-
+[ApiController]
+[Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
-    private readonly  ;
+    private readonly IMediator _mediator ;
 
-    public AuthController()
+    public AuthController(IMediator mediator)
     {
-        
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterCommand command)
+    {
+        var result = await _mediator.Send(command);
+        if (result.Errors.Length != 0) return BadRequest(result.Errors);
+        return Ok(new { Token = result.JwtToken });
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginCommand command)
+    {
+        var result = await _mediator.Send(command);
+        if (result.Errors.Length != 0) return BadRequest(result.Errors);
+        return Ok(new { Token = result.JwtToken });
     }
 }
