f5bf53f28e5013e6df9999785e12283216e70f5d
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-09 14:50:53 +0200

    Implemented command for login

---
+++ src/CarBiddingPlatform.Application/Commands/Auth/Login/LoginCommand.cs
@@ -0,0 +1,9 @@
+﻿using CarBiddingPlatform.Application.DTOs;
+using MediatR;
+
+namespace CarBiddingPlatform.Application.Commands.Auth.Login;
+
+public record LoginCommand(
+    string Email,
+    string Password
+    ) : IRequest<AuthResponse>;
