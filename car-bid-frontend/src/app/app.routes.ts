92ea37d622a43fa89fa87728cb761d9907126a1c
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-15 13:38:18 +0200

    Created authguard

--- car-bid-frontend/src/app/app.routes.ts
+++ car-bid-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AuctionDetailComponent } from './features/auction/auction-detail/auction-detail';
 import { LoginComponent } from './features/auth/login/login';
 import { RegisterComponent } from './features/auth/register/register';
-
+import {DashboardComponent} from './features/home/dashboard/dashboard'
+import {authGuard} from './core/guards/auth-guard';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'auction/:id', component: AuctionDetailComponent },
+  { path: 'auction/:id', component: AuctionDetailComponent, canActivate:[authGuard]},
+  {path: 'home', component: DashboardComponent, canActivate:[authGuard]},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
