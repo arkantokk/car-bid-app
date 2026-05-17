d8505edb698116b477d799bd4825c779b8b9fcca
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-05-17 18:31:08 +0200

    Dashboard functionality improvment

--- car-bid-frontend/src/app/features/home/dashboard/dashboard.ts
+++ car-bid-frontend/src/app/features/home/dashboard/dashboard.ts
@@ -1,16 +1,17 @@
 import {Component, inject, OnInit, signal} from '@angular/core';
-import {AuctionDetails, AuctionService} from '../../../core/services/auctionService';
+import {AuctionDetails, AuctionList, AuctionService} from '../../../core/services/auctionService';
 import {RouterLink} from '@angular/router';
+import {CurrencyPipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe, DatePipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class DashboardComponent implements OnInit {
   private auctionService = inject(AuctionService);
-  activeAuctions = signal<AuctionDetails[]>([]);
+  activeAuctions = signal<AuctionList[]>([]);
 
   ngOnInit() {
     this.auctionService.getAllAuctions().subscribe(auctions => {
